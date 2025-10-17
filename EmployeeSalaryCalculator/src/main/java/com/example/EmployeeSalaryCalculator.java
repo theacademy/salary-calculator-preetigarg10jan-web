994f49b4375c19fd666f76cf0f3c14d4852f3fdd
preetiagrawal <preetigarg10jan@gmail.com>
2025-10-17 21:02:15 +0530

    commit1: updating salary calculator function

--- EmployeeSalaryCalculator/src/main/java/com/example/EmployeeSalaryCalculator.java
+++ EmployeeSalaryCalculator/src/main/java/com/example/EmployeeSalaryCalculator.java
@@ -56,21 +56,31 @@ public class EmployeeSalaryCalculator
 		*/
 
 
-				if(HW <= standardWorkingHours)
-				{
-					//YOUR CODE STARTS HERE
-					 
+		if (HW <= standardWorkingHours)
+		{
+			//YOUR CODE STARTS HERE
 
-					//YOUR CODE ENDS HERE
-				}
-				else
-				{
-					//YOUR CODE STARTS HERE
- 
+			basicPay = HW * HR;
+			overTime = 0;
+			overTimeRate = 0;
+			overTimePay = 0;
+			grossSalary = basicPay;
 
-					//YOUR CODE ENDS HERE
-				}
-				return grossSalary;
+			//YOUR CODE ENDS HERE
+
+		} else {
+			//YOUR CODE STARTS HERE
+
+			basicPay = standardWorkingHours * HR;
+			overTime = HW - standardWorkingHours;
+			overTimeRate = HR * 2;
+			overTimePay = overTime * overTimeRate;
+			grossSalary = basicPay + overTimePay;
+
+			//YOUR CODE ENDS HERE
+
+		}
+		return grossSalary;
 	}
 	
 	static void displayPaySlip()
